dd7819626517360a391581a7ab385bb83756cc0e
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-12-19 20:49:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+objects = [];
+video = "";
+statuss = "";
+
+function modelloaded(){
+    console.log("modelloaded");
+    statuss = true;
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects"; 
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    if(statuss != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
